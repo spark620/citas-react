44d48bfbc6ce6a529d221b302a38f6517201b91b
saac <sparkhk09@gmail.com>
2024-03-07 09:40:11 -0600

    no fue mucho trabajo

--- src/components/Heder.jsx
+++ src/components/Heder.jsx
@@ -2,7 +2,7 @@ function Header(){
     return(
     
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto" >
-        Seguimientos Pacientes {""}
+        Seguimientos Pacientes {""} Con Github {''}
         <span className="text-indigo-600">Veterinaria</span> 
         </h1>
         
